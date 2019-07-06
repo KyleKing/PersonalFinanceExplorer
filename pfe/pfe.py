4ecc666909a012896437e5dc77643eb7592f843c
Kyle King <KyleKing@users.noreply.github.com>
2019-07-06 17:56:06 -0400

    New: WIP left-side tab layout

---
+++ pfe/pfe.py
@@ -0,0 +1,258 @@
+"""Example using Plotly Express with tabbed interface.
+
+Examples: https://www.plotly.express/
+Docs: https://www.plotly.express/plotly_express/
+
+"""
+
+import copy
+from collections import OrderedDict
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly_express as px
+from dash.dependencies import Input, Output
+from dash_charts.helpers import MinGraph, ddOpts, initApp
+
+# ======================================================================================================================
+# Create classes to manage tabs state. Easy to scale up or down
+# >> Demo uses sample data. User could replace with data loaded from a static CSV file, TinyDB, SQLite, etc.
+
+
+class TabBase:
+    """Base tab class with helper methods."""
+
+    title = ''
+    takesArgs = True
+    data = pd.DataFrame()
+    funcMap = OrderedDict([])
+    dims = tuple()
+    dimsDict = OrderedDict([])
+
+    def __init__(self):
+        """Resolve higher-order data members."""
+        self.colOpts = tuple([ddOpts(_c, _c) for _c in self.data.columns])
+        self.funcOpts = tuple([ddOpts(lbl, lbl) for idx, lbl in enumerate(self.funcMap.keys())])
+
+
+class Tab1(TabBase):
+    """Tab1 properties."""
+    title = 'Tip Data'
+    data = px.data.tips()
+    funcMap = OrderedDict([
+        ('scatter', px.scatter),
+        ('density_contour', px.density_contour),
+    ])
+    dims = ('x', 'y', 'color', 'facet_row', 'facet_col')
+    dimsDict = OrderedDict([
+        ('marginal_x', ('histogram', 'rag', 'violin', 'box')),
+        ('marginal_y', ('histogram', 'rag', 'violin', 'box')),
+        ('trendline', ('ols', 'lowess')),
+        # Consider: 'log_y'/'log_x' > daq.BooleanSwitch(id='{}-bool', on=False)
+        # Won't work: error_x..., animation_frame/animation_group, category_orders, labels, etc.
+    ])
+
+
+class Tab2(TabBase):
+    """Tab2 properties."""
+    title = 'Iris Data'
+    data = px.data.iris()
+    funcMap = OrderedDict([
+        ('histogram', px.histogram),
+        ('density_contour', px.density_contour),
+        ('strip', px.strip),
+        ('box', px.box),
+        ('violin', px.violin),
+        ('scatter', px.scatter),
+    ])
+    dims = ('x', 'y', 'color', 'facet_col', 'facet_row')
+
+
+class Tab3(TabBase):
+    """Tab3 properties."""
+    title = 'Gapminder Data'
+    data = px.data.gapminder()
+    funcMap = OrderedDict([
+        ('area', px.area),
+    ])
+    dims = ('x', 'y', 'color', 'line_group', 'facet_row', 'facet_col')
+
+
+class Tab4(TabBase):
+    """Tab4 properties."""
+    title = 'Ternary'
+    data = px.data.election()
+    funcMap = OrderedDict([
+        ('scatter_ternary', px.scatter_ternary),
+        ('line_ternary', px.line_ternary),
+    ])
+    dims = ('a', 'b', 'c', 'color', 'size')
+
+
+class Tab5(TabBase):
+    """Tab5 properties."""
+    title = 'Polar'
+    data = px.data.wind()
+    funcMap = OrderedDict([
+        ('scatter_polar', px.scatter_polar),
+        ('line_polar', px.line_polar),  # (line_close=True)
+    ])
+    dims = ('r', 'theta', 'color')
+
+
+class Tab6(TabBase):
+    """Tab6 properties."""
+    title = 'Color Swatches'
+    takesArgs = False
+    funcMap = OrderedDict([
+        ('colors.qualitative', px.colors.qualitative.swatches),
+        ('colors.sequential', px.colors.sequential.swatches),
+        ('colors.diverging', px.colors.diverging.swatches),
+        ('colors.cyclical', px.colors.cyclical.swatches),
+        ('colors.colorbrewer', px.colors.colorbrewer.swatches),
+        ('colors.cmocean', px.colors.cmocean.swatches),
+        ('colors.carto', px.colors.carto.swatches),
+    ])
+
+# ======================================================================================================================
+# Create class for application to control manage variable scopes
+
+
+class PXDemoApp:
+    """Plotly Express Demo application."""
+
+    def __init__(self):
+        """Initialize tabs and application."""
+        self.TABS = [Tab1(), Tab2(), Tab3(), Tab4(), Tab5(), Tab6()]
+        self.TAB_LOOKUP = {_tab.title: _tab for _tab in self.TABS}
+
+        self.app = initApp()
+
+    def run(self, *, debug=True, **kwargs):
+        """Run the application passing any kwargs to dash."""
+        # Suppress callback verification since tab content won't be rendered when callbacks created
+        self.app.config['suppress_callback_exceptions'] = True
+
+        self.createLayout()
+        self._registerTabCB()
+        for tab in self.TABS:
+            self._registerChartCB(tab)
+
+        self.app.run_server(debug=debug, **kwargs)
+
+    def createLayout(self):
+        """Create layout."""
+        # Style tabs compactly
+        tabStyle = {
+            'padding-top': '10px',
+            'padding-bottom': '10px',
+        }
+        tabStyleSelec = copy.copy(tabStyle)
+        tabStyleSelec['border-left'] = '3px solid #119DFF'
+        self.app.layout = html.Div(children=[
+            html.Div(children=[
+                dcc.Tabs(
+                    id='tabs-select', value=self.TABS[0].title, vertical=True,
+                    children=[
+                        dcc.Tab(label=_tab.title, value=_tab.title, style=tabStyle, selected_style=tabStyleSelec) for _tab in self.TABS
+                    ],
+                    style={'width': '100%'},
+                    parent_style={'width': '100%'},
+                ),
+            ], style={
+                'background-color': '#F9F9F9',
+                'padding': '15px 0 0 5px',
+                'position': 'fixed', 'left': '0', 'top': '0', 'bottom': '0',
+                'width': '9%',
+            }),
+            html.Div(style={'margin-left': '10%', 'width': '90%'}, children=[
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+                html.H1('Dash/Plotly Express Data Exploration Demo'),
+
+                html.Div(id='tabs-content'),
+            ]),
+        ])
+        # Configure each tab
+        self.__createTabMap()
+
+    def __createTabMap(self):
+        """Create TAB_MAP to store the tab individual tab layouts."""
+        self.TAB_MAP = {
+            tab.title: html.Div([
+                html.Div([
+                    html.P([
+                        'Plot Type:',
+                        dcc.Dropdown(
+                            id='{}-func'.format(tab.title), options=tab.funcOpts, value=tab.funcOpts[0]['label']
+                        ),
+                    ])
+                ] + [
+                    html.P([
+                        _d + ':', dcc.Dropdown(id='{}-{}'.format(tab.title, _d), options=tab.colOpts)
+                    ]) for _d in tab.dims
+                ] + [
+                    html.P([
+                        _d + ':',
+                        dcc.Dropdown(id='{}-{}'.format(tab.title, _d),
+                                     options=[ddOpts(_l, _l) for _l in _list]),
+                    ]) for _d, _list in tab.dimsDict.items()
+                ], style={'width': '25%', 'float': 'left'},
+                ),
+                MinGraph(id='{}-graph'.format(tab.title), style={'width': '75%', 'display': 'inline-block'}),
+            ])
+            for tab in self.TABS
+        }
+
+    def _registerTabCB(self):
+        """Register callback to handle tab rendering."""
+        @self.app.callback(
+            Output('tabs-content', 'children'),
+            [Input('tabs-select', 'value')],
+        )
+        def renderTabs(tabName):
+            """Render tabs when switched."""
+            return self.TAB_MAP[tabName]
+
+    def _registerChartCB(self, tab):
+        """Register the callbacks for handling the user input panel and chart rendering.
+
+        tab -- tab dictionary.
+
+        """
+        inputs = [Input('tabs-select', 'value'), Input('{}-func'.format(tab.title), 'value')]
+        inputs.extend([Input('{}-{}'.format(tab.title, _d), 'value') for _d in tab.dims])
+        inputs.extend([Input('{}-{}'.format(tab.title, _k), 'value') for _k in tab.dimsDict.keys()])
+
+        # Register callbacks for each tab
+        @self.app.callback(
+            Output('{}-graph'.format(tab.title), 'figure'),
+            inputs,
+        )
+        def renderFigure(tabName, nameFunc, *args):
+            """Create the figure."""
+            # Check if the trigger event is a tab change. If so, return an empty chart
+            ctx = dash.callback_context
+            if 'tabs-select.value' in [_t['prop_id'] for _t in ctx.triggered]:
+                return {}
+            # Otherwise, parse the arguments to generate a new plot
+            _tab = self.TAB_LOOKUP[tabName]
+            if tab.takesArgs:
+                keys = list(_tab.dims) + list(_tab.dimsDict.keys())
+                kwargs = OrderedDict(zip(keys, args))
+                return _tab.funcMap[nameFunc](_tab.data, height=650, **kwargs)
+            return _tab.funcMap[nameFunc]()
